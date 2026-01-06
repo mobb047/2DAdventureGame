720807f1a234335aa81f4861f83e8d218d370628
mobb047 <g17835348699@outlook.com>
2026-01-07 02:18:32 +0800

    Initial commit: Scripts

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerInputControl inputControl;
+    public Rigidbody2D rb;
+    public Vector2 inputDirection;
+    public float speed;
+
+    //一些生命周期函数，我也不太清楚叫啥。awake是最先的对象创建时候，每个对象只执行一次，
+    //start是第二个的,在创建对象时候，每个对象只执行一次，
+    // onenable是在对象启用时候，每次启用都会执行，
+    // ondisable是在对象禁用时候，每次禁用都会执行，
+    // ondestroy是最后一个对象销毁时候，每个对象只执行一次，
+    // update是每帧调用的，
+    // fixedupdate是每固定帧调用的，
+    // lateupdate是每帧最后调用的
+    private void Awake()
+    {
+        inputControl = new PlayerInputControl();
+    }
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+    private void Update()//输入处理
+    {
+        inputDirection = inputControl.GamePlay.Move.ReadValue<Vector2>();
+    }
+    private void FixedUpdate()//物理移动，键盘wasd移动
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        rb.velocity = new Vector2(inputDirection.x * speed, rb.velocity.y);
+
+        int faceDir = (int)transform.localScale.x;//这里在float类型前面加上（int）表示强制转换成int类型
+
+        if (inputDirection.x < 0)
+            faceDir = -1;
+        else if (inputDirection.x > 0)
+            faceDir = 1;
+        //下面是人物翻转,大写的是类型，小写的是变量，这里的transform就是commponent里的transform，
+        // 要修改的是transform.localScale，localscale是一个三维向量，有xyz，向左-1，向右1，
+        // 在键盘输入时确实是-1和1，但是如果是手柄输入的话，可能是0.5之类的，这里在上面确定一个新变量faceDir，
+        // 然后根据输入方向来确定这个变量的值，然后根据这个变量的值来修改transform.localScale,
+        // 当输入的的x值小于0，就改成-1，大于0，就是1
+        transform.localScale = new Vector3(faceDir, 1, 1);//y and z are the same=1，
+        // 什么时候可以确定使用localscale的方法呢？人物图片锚点的位置决定人物的反转方向，锚点在人物中间才可以用。
+        // 另外，当scale默认是1，1，1时候，如果手动改变了人物的比例比如改为2，代码中的1就要改为2，faceDir和y,z都要变为2，
+        // 还有sprite组件，可以用sprite renderer.flipx=ture/false来翻转
+    }
+
+}
